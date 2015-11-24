661805275c3691f0026baa17714f87bb1e9629d6
Eric Malloy <eric.malloy@pearson.com>
2015-11-24 15:33:36 -0500

    packer getting passwdless sudo for various puppet manifest mounts. @ToDo - make a more secure way of transport without passwordless anything

--- scripts/vagrant-ubuntu.sh
+++ scripts/vagrant-ubuntu.sh
@@ -7,6 +7,13 @@ mv authorized_keys /home/vagrant/.ssh
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
 
+# need vagrant having passwdless sudo
+# @ToDo - tidy this up.
+
+echo "vagrant ALL=(ALL) NOPASSWD:ALL
+Defaults:vagrant !requiretty" \
+> /etc/sudoers.d/vagrant
+
 aptitude update
 aptitude upgrade -y
 aptitude install -y net-tools bind-utils nfs-common portmap rpcbind libgssglue nfs-utils keyutils libevent nfs-utils-lib
